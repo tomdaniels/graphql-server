4ef13a949d40ffc48ab820264013b6121d032de0
Tommy Daniels <thomson.daniels@domain.com.au>
2020-10-14 13:35:30 +1100

    moxks & resolvers - easily iterate on schema designs

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
-console.log('helloworld');
+const { ApolloServer, gql } = require('apollo-server');
+
+const typeDefs = gql`
+    type Query {
+        hello: String
+        helloTwo: String
+    }
+`;
+
+const mocks = {
+    String: () => "hello",
+};
+
+const resolvers = {
+    Query: {
+        helloTwo: () => 'Real hello'
+    }
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    mocks,
+    resolvers,
+    mockEntireSchema: false,
+});
+
+server.listen().then(({ url }) => {
+    console.log(`🚀 Server ready at ${url}`)
+});
